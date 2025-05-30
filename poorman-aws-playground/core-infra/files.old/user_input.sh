ab5ae266b6963a691bc9e0b655103069bb8a3918
Luca Esposito <luca.esposito@lastminute.com>
2025-05-30 21:01:54 +0200

    feat(cleanup): remove useless files

--- poorman-aws-playground/core-infra/files.old/user_input.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Please type 'yes' to proceed: "
-read input
-
-# Check if the input is "yes"
-if [ "$input" != "yes" ] || [ -z "$input" ]; then
-  echo "Deployment aborted. Input not recognized."
-  exit 1  # Force an error to stop Terraform from proceeding
-fi
